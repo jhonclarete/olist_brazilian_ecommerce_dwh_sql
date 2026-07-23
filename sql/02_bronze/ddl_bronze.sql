ab8df3bb9f774fe33e572fa261e061b6b404702f
jhonrommelc <jhonrommel.clarete@gmail.com>
2026-07-23 17:19:14 +0800

    update length of some columns

--- sql/02_bronze/ddl_bronze.sql
+++ sql/02_bronze/ddl_bronze.sql
@@ -1,6 +1,6 @@
 -- This script creates tables in the 'bronze' schema, dropping existing tables if they already exist.
 
-USE olist_brazilian_ecommerce_dwh
+USE olist_brazilian_ecommerce_dwh;
 GO
 
 IF OBJECT_ID('bronze.olist_customers_dataset', 'U') IS NOT NULL
@@ -13,7 +13,7 @@ CREATE TABLE bronze.olist_customers_dataset
 	customer_id NVARCHAR(50),
 	customer_unique_id NVARCHAR(50),
 	customer_zip_code_prefix NVARCHAR(5),
-	customer_city NVARCHAR(30),
+	customer_city NVARCHAR(50),
 	customer_state NVARCHAR(2)
 );
 GO
@@ -28,7 +28,7 @@ CREATE TABLE bronze.olist_geolocation_dataset
 	geolocation_zip_code_prefix NVARCHAR(5),
 	geolocation_lat DECIMAL(18, 15),
 	geolocation_lng DECIMAL(18, 15),
-	geolocation_city NVARCHAR(30),
+	geolocation_city NVARCHAR(50),
 	geolocation_state NVARCHAR(2)
 );
 GO
@@ -75,7 +75,7 @@ CREATE TABLE bronze.olist_order_reviews_dataset
 	review_id NVARCHAR(50),
 	order_id NVARCHAR(50),
 	review_score INT,
-	review_comment_title NVARCHAR(20),
+	review_comment_title NVARCHAR(40),
 	review_comment_message NVARCHAR(400),
 	review_creation_date DATE,
 	review_answer_timestamp DATETIME2
@@ -108,7 +108,7 @@ END;
 CREATE TABLE bronze.olist_products_dataset
 (
 	product_id NVARCHAR(50),
-	product_category_name NVARCHAR(30),
+	product_category_name NVARCHAR(50),
 	product_name_lenght INT,
 	product_description_lenght INT,
 	product_photos_qty INT,
@@ -128,7 +128,7 @@ CREATE TABLE bronze.olist_sellers_dataset
 (
 	seller_id NVARCHAR(50),
 	seller_zip_code_prefix NVARCHAR(5),
-	seller_city NVARCHAR(30),
+	seller_city NVARCHAR(50),
 	seller_state NVARCHAR(2)
 );
 GO
