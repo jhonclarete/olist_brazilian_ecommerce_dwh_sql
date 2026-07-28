2b26af635b6690c5ec83e556f856a0df247544f4
jhonrommelc <jhonrommel.clarete@gmail.com>
2026-07-28 14:40:26 +0800

    update category and qeolocation quality chekcs

--- sql/03_silver/quality_checks/olist_geolocation_dataset_quality_checks.sql
+++ sql/03_silver/quality_checks/olist_geolocation_dataset_quality_checks.sql
@@ -1,144 +1,94 @@
 USE olist_brazilian_ecommerce_dwh;
 GO
 
--- geolocation_zip_code_prefix
--- Check for NULL Values
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_zip_code_prefix IS NULL
-
--- Check blank values
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE TRIM(geolocation_zip_code_prefix) = ''
-
--- Check for whitespaces
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_zip_code_prefix != TRIM(geolocation_zip_code_prefix);
-
--- Check ZIP prefix length; Brazilian CEP prefix should have 5 digits.
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE LEN(geolocation_zip_code_prefix) != 5
-
--- Check for non-numeric values
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_zip_code_prefix LIKE '%[^0-9]%'
-
--- Check duplicate record
-SELECT
+-- Check for duplicate record
+-- ERROR
+SELECT 
     geolocation_zip_code_prefix,
     geolocation_lat,
-    geolocation_lng,
-    LOWER(TRIM(geolocation_city)) COLLATE Latin1_General_100_CI_AI AS city_normalized,
-    geolocation_state,
-    COUNT(*) AS cnt
+    geolocation_lng, 
+    geolocation_city COLLATE Latin1_General_CI_AI AS geolocation_city,
+    geolocation_state, 
+    COUNT(*)
 FROM silver.olist_geolocation_dataset
-GROUP BY
+GROUP BY 
     geolocation_zip_code_prefix,
     geolocation_lat,
-    geolocation_lng,
-    LOWER(TRIM(geolocation_city)) COLLATE Latin1_General_100_CI_AI,
+    geolocation_lng, 
+    geolocation_city COLLATE Latin1_General_CI_AI,
     geolocation_state
-HAVING COUNT(*) > 1;
+HAVING
+    COUNT(*) > 1
 
-
--- geolocation_lat
--- Check NULLs
+-- Check if Lat and Lng are valid values
 SELECT *
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_lat IS NULL
+WHERE
+    (CAST(geolocation_lat AS decimal(19, 15)) < -90 AND CAST(geolocation_lat AS decimal(19, 15)) > 90)
+    OR (CAST(geolocation_lng AS decimal(19, 15)) < -180 AND CAST(geolocation_lng AS decimal(19, 15)) > 180)
+    OR geolocation_lat IS NULL
+    or geolocation_lng is NULL
 
--- Check Lat range
+-- Check if Lat are within Brazil
 SELECT *
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_lat < -33.75
-OR geolocation_lat > 5.27;
+WHERE CAST(geolocation_lat AS decimal(19, 15)) < -33.75 OR CAST(geolocation_lat AS decimal(19, 15)) > 5.27
 
--- Check 0 coordinate
+
+-- Check if Lng are within Brazil
 SELECT *
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_lat = 0
+WHERE CAST(geolocation_lng AS decimal(19, 15)) < -73.98 OR CAST(geolocation_lng AS decimal(19, 15)) > -28.85
 
-
--- geolocation_lng
--- Check NULLs
-SELECT *
+-- Check for Nulls or Empty data or unwanted spaces
+SELECT * 
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_lng IS NULL
+WHERE 
+    geolocation_city IS NULL
+    OR TRIM(geolocation_city) = ''
+    OR geolocation_city != TRIM(geolocation_city)
 
--- Check Lng range
-SELECT *
+select * from silver.olist_geolocation_dataset
+where TRIM(geolocation_city) LIKE '%[0-9]%'
+
+-- Check if city is in Lower Case
+SELECT * 
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_lng < -73.99
-OR geolocation_lng > -34.79;
+WHERE geolocation_city COLLATE Latin1_General_CS_AS != LOWER(geolocation_city)
 
--- Check 0 coordinate
-SELECT *
+-- Check if state is in Upper Case
+SELECT * 
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_lng = 0
+WHERE geolocation_state COLLATE Latin1_General_CS_AS != UPPER(geolocation_state)
 
-
--- geolocation_city
--- Check NULLs
+-- Check if state is valid
 SELECT *
+from silver.olist_geolocation_dataset
+WHERE geolocation_state NOT IN ('RS','CE','PE','AL','PI','MG','DF','TO',
+'RR','SE','PA','GO','RO','ES','RJ','AC','SP','AM',
+'PR','MT','PB','MA','AP','MS','SC','RN','BA')
+
+-- Check for validity of Prefix code
+SELECT * 
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_city IS NULL
+WHERE LEN(TRIM(geolocation_zip_code_prefix)) != 5
 
--- Check blank values
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE TRIM(geolocation_city) = ''
-
--- Check leading/trailing spaces
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_city != TRIM(geolocation_city);
-
--- Check for uppercases
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_city COLLATE Latin1_General_CS_AS = UPPER(geolocation_city);
-
--- Check for invalid character
-SELECT *
+-- Check for mojibake text
+SELECT distinct geolocation_city
 FROM silver.olist_geolocation_dataset
 WHERE geolocation_city LIKE '%[0-9]%';
 
-
--- geolocation_state
--- Check NULLs
-SELECT *
+SELECT DISTINCT 
+    geolocation_zip_code_prefix,
+    geolocation_city,
+    geolocation_state
 FROM silver.olist_geolocation_dataset
-WHERE geolocation_state IS NULL
+WHERE 
+    -- 1. Finds capital letters or accents glued inside a lowercase sequence (e.g., "MaceiÃ³")
+    geolocation_city COLLATE Latin1_General_BIN LIKE '%[a-z][ÃÂÊÓãéíóúç]% '
+    -- 2. Finds loose floating mathematical/subscript characters 
+    OR geolocation_city LIKE '%[³²ºª¶§†‡]%'
+    -- 3. Finds broken multi-byte question marks or text boxes
+    OR geolocation_city LIKE '%?%';
 
--- Check blank values
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE TRIM(geolocation_state) = ''
-
--- Check for whitespaces
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_state != TRIM(geolocation_state);
-
--- Check for lowercases
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_state COLLATE Latin1_General_CS_AS = LOWER(geolocation_state);
-
--- Check length
-SELECT *
-FROM silver.olist_geolocation_dataset
-WHERE LEN(geolocation_state) != 2;
-
--- Find invalid state codes
-SELECT DISTINCT geolocation_state
-FROM silver.olist_geolocation_dataset
-WHERE geolocation_state NOT IN (
-'AC','AL','AP','AM','BA','CE','DF','ES','GO',
-'MA','MT','MS','MG','PA','PB','PR','PE','PI',
-'RJ','RN','RS','RO','RR','SC','SP','SE','TO'
-);
+    
--- sql/03_silver/quality_checks/product_category_name_translation_quality_checks.sql
+++ sql/03_silver/quality_checks/product_category_name_translation_quality_checks.sql
@@ -1,60 +1,29 @@
 USE olist_brazilian_ecommerce_dwh;
 GO
 
--- Check if there are mojibake
-SELECT * 
-FROM silver.olist_product_category_name_translation_dataset
-WHERE REGEXP_LIKE(product_category_name_english, '(Ã[a-zA-Z0-9]|â€|æ—)', 'c'); 
-
--- product_category_name
--- Check Nulls
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE product_category_name IS NULL
-
--- Check blank values
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE TRIM(product_category_name) = ''
-
--- Check for whitespaces
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE product_category_name != TRIM(product_category_name)
-
--- Check for upperrcases
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE product_category_name COLLATE Latin1_General_CS_AS = UPPER(product_category_name);
-
 -- Check for duplicates
-SELECT COUNT(*) FROM silver.olist_product_category_name_translation_dataset
-GROUP BY product_category_name
+SELECT 
+    product_category_name COLLATE Latin1_General_100_CI_AI AS product_category_name, 
+    product_category_name_english COLLATE Latin1_General_100_CI_AI AS product_category_name_english, COUNT(*) 
+FROM silver.olist_product_category_name_translation_dataset
+GROUP BY 
+    product_category_name COLLATE Latin1_General_100_CI_AI, 
+    product_category_name_english COLLATE Latin1_General_100_CI_AI
 HAVING COUNT(*) > 1
 
-
--- product_category_name_english
--- Check Nulls
-SELECT *
+SELECT 
+    product_category_name_english COLLATE Latin1_General_100_CI_AI AS product_category_name_english, 
+    product_category_name COLLATE Latin1_General_100_CI_AI AS product_category_name, COUNT(*) 
 FROM silver.olist_product_category_name_translation_dataset
-WHERE product_category_name_english IS NULL
-
--- Check blank values
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE TRIM(product_category_name_english) = ''
-
--- Check for whitespaces
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE product_category_name_english != TRIM(product_category_name_english)
-
--- Check for upperrcases
-SELECT *
-FROM silver.olist_product_category_name_translation_dataset
-WHERE product_category_name_english COLLATE Latin1_General_CS_AS = UPPER(product_category_name_english);
-
--- Check for duplicates
-SELECT COUNT(*) FROM silver.olist_product_category_name_translation_dataset
-GROUP BY product_category_name_english
+GROUP BY 
+    product_category_name_english COLLATE Latin1_General_100_CI_AI, 
+    product_category_name COLLATE Latin1_General_100_CI_AI
 HAVING COUNT(*) > 1
+
+-- Check for Nulls or empty
+SELECT 
+    * 
+FROM silver.olist_product_category_name_translation_dataset 
+WHERE 
+    product_category_name IS NULL 
+    OR TRIM(product_category_name_english) = ''
