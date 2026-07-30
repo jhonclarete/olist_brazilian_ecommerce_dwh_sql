18dde21803cd41a1f3e5e8b90277c8e88d625f20
jhonclarete <jhonrommel.clarete@gmail.com>
2026-07-30 21:00:50 +0800

    update orders quality checks

--- sql/03_silver/quality_checks/olist_orders_dataset_quality_checks.sql
+++ sql/03_silver/quality_checks/olist_orders_dataset_quality_checks.sql
@@ -1,208 +1,69 @@
 USE olist_brazilian_ecommerce_dwh;
 GO
 
--- order_id
--- Check for Nulls
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_id IS NULL
-
--- Check blank values
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE TRIM(order_id) = ''
-
--- Check for whitespaces
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_id != TRIM(order_id)
-
--- Check length of seller_id
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE LEN(order_id) != 32
-
--- Check for non-numeric values
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_id LIKE '%^[0-9a-f]{32}$%'
-
 -- Check for duplicates
-SELECT COUNT(*) FROM silver.olist_orders_dataset
+SELECT 
+    order_id, 
+    COUNT(*)
+FROM bronze.olist_orders_dataset
 GROUP BY order_id
-HAVING COUNT(*) > 1
+HAVING COUNT(*) > 1 OR order_id IS NULL
 
-
--- customer_id
--- Check for Nulls
+-- Check for whitespaces or blanks
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE customer_id IS NULL
+FROM bronze.olist_orders_dataset
+WHERE 
+    TRIM(order_status) = ''
+    OR order_status != TRIM(order_status)
 
--- Check blank values
+-- Check for nulls
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE TRIM(customer_id) = ''
-
--- Check for whitespaces
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE customer_id != TRIM(customer_id)
-
--- Check length of seller_id
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE LEN(customer_id) != 32
-
--- Check for non-numeric values
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE customer_id LIKE '%^[0-9a-f]{32}$%'
-
--- Check for duplicates
-SELECT COUNT(*) FROM silver.olist_orders_dataset
-GROUP BY customer_id
-HAVING COUNT(*) > 1
-
--- Check if customer_id not exists in customer dataset
-SELECT *
-FROM silver.olist_orders_dataset o
-WHERE NOT EXISTS (SELECT 1 FROM silver.olist_customers_dataset c WHERE c.customer_id = o.customer_id)
-
-
--- order_status
--- Check for Nulls
-SELECT *
-FROM silver.olist_orders_dataset
+FROM bronze.olist_orders_dataset
 WHERE order_status IS NULL
 
--- Check invalid status value
+-- Check if customer_id exists in Customer Table
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_status NOT IN ('approved', 'canceled',
-'created',
-'delivered',
-'invoiced',
-'processing',
-'shipped',
-'unavailable')
+FROM bronze.olist_orders_dataset o
+WHERE NOT EXISTS (SELECT 1 FROM silver.olist_customers_dataset c WHERE c.customer_id = o.customer_id)
 
--- Check for upperrcases
+-- Check if order_status not in lower case
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_status COLLATE Latin1_General_CS_AS = UPPER(order_status);
+FROM bronze.olist_orders_dataset
+WHERE order_status COLLATE Latin1_General_CS_AS != LOWER(order_status);
 
-
--- order_purchase_timestamp
-
--- Check for Nulls
+-- Missing timestamp checks
 SELECT *
-FROM silver.olist_orders_dataset
+FROM bronze.olist_orders_dataset
 WHERE order_purchase_timestamp IS NULL
 
--- Check for datetime format
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE TRY_CAST(order_purchase_timestamp AS DATETIME2) IS NULL
-
--- Should not be before Olist business start date (2016)
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE YEAR(order_purchase_timestamp) < 2016
-
--- Cannot be greater than current date
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_purchase_timestamp > GETDATE()
+FROM bronze.olist_orders_dataset
+WHERE 
+    order_approved_at IS NULL
+    AND order_status NOT IN ('canceled', 'unavailable', 'created')
 
 
--- order_approved_at
--- Must be >= order_purchase_timestamp
+
+
+
+
+-- Approval timestamp cannot be earlier than purchase timestamp
 SELECT *
-FROM silver.olist_orders_dataset
+FROM bronze.olist_orders_dataset
 WHERE order_approved_at < order_purchase_timestamp
 
--- Cannot be greater than current date
+-- Carrier delivery date cannot be earlier than purchase date
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_approved_at > GETDATE()
+FROM bronze.olist_orders_dataset
+WHERE order_delivered_carrier_date < order_purchase_timestamp
 
--- Not Null if status is approved
+-- Customer delivery date cannot be earlier than shipped date
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_status = 'approved' AND order_approved_at IS NULL
-
--- Check for datetime format
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE TRY_CAST(order_approved_at AS DATETIME2) IS NULL AND order_status = 'approved'
-
-
--- order_delivered_carrier_date
--- Should exist when status is delivered or shipped
--- ERROR
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE (order_status = 'delivered' OR order_status = 'shipped') AND order_delivered_carrier_date IS NULL
-
--- Must be >= order_approved_at
--- ERROR
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_delivered_carrier_date < order_approved_at
-
--- Cannot be greater than current date
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_delivered_carrier_date > GETDATE()
-
--- Check for datetime format
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_delivered_carrier_date IS NULL AND (order_status = 'delivered' OR order_status = 'shipped')
-
-
--- order_delivered_customer_date
--- Should exist only when status = delivered
--- ERROR
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE (order_status = 'delivered') AND order_delivered_customer_date IS NULL
-
--- Must be >= order_delivered_carrier_date
--- ERROR
-SELECT *
-FROM silver.olist_orders_dataset
+FROM bronze.olist_orders_dataset
 WHERE order_delivered_customer_date < order_delivered_carrier_date
 
--- Cannot be greater than current date
+-- Estimated delivery date should be after purchase date
 SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_delivered_customer_date > GETDATE()
+FROM bronze.olist_orders_dataset
+WHERE order_delivered_customer_date < order_delivered_carrier_date
 
--- Delivery date should not be before purchase date
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_delivered_customer_date < order_purchase_timestamp
-
-
--- order_estimated_delivery_date
--- Must not be NULL
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_estimated_delivery_date IS NULL
-
--- Should be >= purchase date
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_estimated_delivery_date < order_purchase_timestamp
-
--- Estimated delivery >= Actual delivery (when delivered)
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE order_estimated_delivery_date < order_delivered_customer_date
-
--- Check for datetime format
-SELECT *
-FROM silver.olist_orders_dataset
-WHERE TRY_CAST(order_estimated_delivery_date AS DATETIME2) IS NULL
